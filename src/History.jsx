19dc8d5603722ddc78d3557bca46cf1566f8a593
RemondJu <remond.neiluj@gmail.com>
2018-10-08 19:16:53 +0200

    quest done

---
+++ src/History.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class History extends Component {
+    state = {  }
+    render() { 
+        return ( <h2>Notre histoire fascinante...</h2> );
+    }
+}
+ 
+export default History ;
---
+++ src/Home.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class Home extends Component {
+    state = {  }
+    render() { 
+        return ( <h1>Bienvenue sur l'accueil les loulous !</h1> );
+    }
+}
+ 
+export default Home;
